3e4e06e728399ec178a332ef8cb042aec7db570b
aiyakunichkin <aiakunichkin@amazinghiring.com>
2016-12-29 12:57:10 +0300

    Написание теста создания группы

---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,48 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class GroupCreationTests {
+
+    @BeforeMethod
+    public void setUp(){
+        open("http://127.0.0.1:8080/addressbook/");
+        login("admin", "secret");
+    }
+
+    @Test
+    public void testGroupCreation(){
+        initGropPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("test1", "test2", "test3"));
+        submitGroupCreation();
+    }
+
+    private void login(String login, String password) {
+        $("input[name='user']").setValue(login);
+        $("input[name='pass']").setValue(password);
+        $("input[type='submit']").click();
+    }
+
+    private void submitGroupCreation() {
+        $("input[name='submit']").click();
+    }
+
+    private void initGroupCreation() {
+        $("input[name='new']").click();
+    }
+
+    private void initGropPage() {
+        $("a[href='group.php']").click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        $("input[name='group_name']").setValue(groupData.getGroupName());
+        $("textarea[name='group_header']").setValue(groupData.getGroupHeader());
+        $("textarea[name='group_footer']").setValue(groupData.getGroupFooter());
+    }
+
+}
