445df2aa1cf4930a14d8f13c5a39f9ab6b427a13
aiyakunichkin <aiakunichkin@amazinghiring.com>
2016-12-29 19:48:23 +0300

    Настроить запуск тестов в разных браузерах

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,30 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import com.codeborne.selenide.Configuration;
+
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
 
 public class ApplicationManager {
+
+    String browser = "";
 
     private ContactHelper contactHelper = new ContactHelper();
     private GroupHelper groupHelper = new GroupHelper();
     private NavigationHelper navigationHelper = new NavigationHelper();
     private SessionHelper sessionHelper = new SessionHelper();
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
+        // Для примера использования if else, а вообще я бы написал Configuration.browser = browser;
+        if(browser.equals("chrome")){
+            Configuration.browser = "chrome";
+        } else if (browser.equals("firefox")) {
+            Configuration.browser = "firefox";
+        }
         open("http://127.0.0.1:8080/addressbook/");
         sessionHelper.login("admin", "secret");
     }
