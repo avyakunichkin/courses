c8e93a73771235e621b1b692ff8097f59f7d700d
aiyakunichkin <aiakunichkin@amazinghiring.com>
2017-03-03 21:26:31 +0300

    Добавляем удаленный запуск с помощью Selenium Server

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import static com.codeborne.selenide.Selenide.close;
@@ -29,7 +33,18 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
-        Configuration.browser = browser;
+        if ("".equals(properties.getProperty("selenium.server"))){
+            Configuration.browser = browser;
+        }else{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(System.getProperty("browser"));
+            try {
+                new RemoteWebDriver(new URL("http://172.16.101.70:4444/wd/hub"), capabilities);
+            }
+            catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
         open(properties.getProperty("web.baseUrl"));
         sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -16,7 +16,7 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown(){
         app.stop();
     }
